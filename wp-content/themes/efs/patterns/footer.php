603acc51f9c688d99538c5c00cbc173944f6ea64
Elias Axonov <elias@axonov.com>
2026-03-08 13:24:42 +0100

    uppsättning av startsida och sidfot

---
+++ wp-content/themes/efs/patterns/footer.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: twentytwentyfive/footer
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ * Description: Footer columns with logo, title, tagline and links.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+?>
+<!-- wp:group {"className":"is-style-section-2","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"constrained","contentSize":"","wideSize":"1200px"}} -->
+<div class="wp-block-group is-style-section-2 has-base-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"25%","fontSize":"medium"} -->
+<div class="wp-block-column has-medium-font-size" style="flex-basis:25%"><!-- wp:site-logo {"width":86} /-->
+
+<!-- wp:group {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-small-font-size" style="font-style:normal;font-weight:700"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
+<p style="font-style:normal;font-weight:700">Bankgiro: 900-9903<br>Swish: 900 99 03<br>Orgnr: 802000-8184</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"accent-3","className":"is-style-outline","style":{"elements":{"link":{"color":{"text":"var:preset|color|accent-3"}}},"spacing":{"padding":{"left":"var:preset|spacing|30","right":"var:preset|spacing|30","top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"typography":{"fontSize":"0.7rem"}}} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-accent-3-color has-text-color has-link-color has-custom-font-size wp-element-button" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--30);font-size:0.7rem">Ge en gåva</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"25%","fontSize":"medium"} -->
+<div class="wp-block-column has-medium-font-size" style="flex-basis:25%"><!-- wp:group {"fontSize":"medium","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-medium-font-size"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|accent-3"}}}},"textColor":"accent-3"} -->
+<p class="has-accent-3-color has-text-color has-link-color" style="font-style:normal;font-weight:700">Kontakt EFS kansli</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
+<p style="font-style:normal;font-weight:700"><strong>Tel: 018–430 25 00</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
+<p style="font-style:normal;font-weight:700">Postadress:<br>Box 23001<br>750 23 Uppsala</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
+<p style="font-style:normal;font-weight:700">Besöksadress:<br>Heidenstamsgatan 75<br>754 27 Uppsala</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"25%","fontSize":"medium"} -->
+<div class="wp-block-column has-medium-font-size" style="flex-basis:25%"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|accent-3"}}}},"textColor":"accent-3"} -->
+<p class="has-accent-3-color has-text-color has-link-color" style="font-style:normal;font-weight:700">Genvägar</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:navigation {"ref":10043,"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|20"},"typography":{"fontStyle":"normal","fontWeight":"700"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"25%"} -->
+<div class="wp-block-column" style="flex-basis:25%"><!-- wp:group {"fontSize":"medium","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-medium-font-size"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|accent-3"}}}},"textColor":"accent-3"} -->
+<p class="has-accent-3-color has-text-color has-link-color" style="font-style:normal;font-weight:700">Sociala Kanaler</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"medium","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-medium-font-size" style="font-style:normal;font-weight:700"><!-- wp:paragraph -->
+<p><a href="https://www.facebook.com/efsvikbolandet" data-type="link" data-id="https://www.facebook.com/efsvikbolandet">Facebook</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":10039,"sizeSlug":"full","linkDestination":"media","style":{"layout":{"selfStretch":"fixed","flexSize":"50px"}}} -->
+<figure class="wp-block-image size-full"><a href="http://127.0.0.1:32773/wp-content/uploads/Swish-Logo-Primary-Dark-BG.png"><img src="http://127.0.0.1:32773/wp-content/uploads/Swish-Logo-Primary-Dark-BG.png" alt="" class="wp-image-10039"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
