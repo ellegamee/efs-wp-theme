9e15f66ac1075220b76ad453db3f3db2dc90284c
Elias Axonov <elias@axonov.com>
2026-06-08 17:07:09 +0200

    feat: add custom search block styles and integrate them into theme assets and editor styles

--- wp-content/themes/efs/include/setup.php
+++ wp-content/themes/efs/include/setup.php
@@ -6,9 +6,16 @@
  */
 
 add_action( 'after_setup_theme', function() {
+	// Enable support for editor styles.
+	add_theme_support( 'editor-styles' );
+
+	// Enqueue editor styles.
 	add_editor_style( 'style.css' );
 } );
 
-add_action( 'wp_enqueue_scripts', function() {
+/**
+ * Enqueue assets for both front-end and editor.
+ */
+add_action( 'enqueue_block_assets', function() {
 	wp_enqueue_style( 'efs-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
 } );
