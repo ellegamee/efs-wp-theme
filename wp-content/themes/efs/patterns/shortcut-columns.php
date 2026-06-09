fcd8809ae97fb027127519ae295d86240d62abdc
Elias Axonov <elias@axonov.com>
2026-06-09 13:02:57 +0200

    feat: update full-width section and shortcut columns for improved layout and styling

--- wp-content/themes/efs/patterns/shortcut-columns.php
+++ wp-content/themes/efs/patterns/shortcut-columns.php
@@ -5,18 +5,15 @@
  * Categories: efs, featured
  */
 ?>
-<!-- wp:group {"backgroundColor":"accent-4","textColor":"accent-1","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-accent-1-color has-accent-4-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)">
-    <!-- wp:columns {"isStackedOnMobile":true} -->
-    <div class="wp-block-columns is-stacked-on-mobile">
-        <!-- wp:column -->
-        <div class="wp-block-column">
-            <!-- wp:group {"className":"is-clickable-shortcut","layout":{"type":"constrained"}} -->
-            <div class="wp-block-group is-clickable-shortcut">
-                <!-- wp:image {"align":"center","sizeSlug":"medium","linkDestination":"none","width":200,"height":200} -->
-                <figure class="wp-block-image aligncenter size-medium"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/calendar-shortcut-v2.png" alt="Kalender"/></figure>
+<!-- wp:group {"align":"full","className":"align-full","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"backgroundColor":"accent-4","textColor":"accent-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull align-full has-accent-1-color has-accent-4-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:columns {"className":"is-stacked-on-mobile"} -->
+    <div class="wp-block-columns is-stacked-on-mobile"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:group {"className":"is-clickable-shortcut","layout":{"type":"constrained"}} -->
+            <div class="wp-block-group is-clickable-shortcut"><!-- wp:image {"sizeSlug":"medium","linkDestination":"none","align":"center"} -->
+                <figure class="wp-block-image aligncenter size-medium"><img src="http://localhost:32781/wp-content/themes/efs/assets/images/calendar-shortcut-v2.png" alt="Kalender" /></figure>
                 <!-- /wp:image -->
-                <!-- wp:paragraph {"align":"center"} -->
+
+                <!-- wp:paragraph {"style":{"typography":{"textAlign":"center"}}} -->
                 <p class="has-text-align-center">Kalender</p>
                 <!-- /wp:paragraph -->
             </div>
@@ -25,13 +22,12 @@
         <!-- /wp:column -->
 
         <!-- wp:column -->
-        <div class="wp-block-column">
-            <!-- wp:group {"className":"is-clickable-shortcut","layout":{"type":"constrained"}} -->
-            <div class="wp-block-group is-clickable-shortcut">
-                <!-- wp:image {"align":"center","sizeSlug":"medium","linkDestination":"none","width":200,"height":200} -->
-                <figure class="wp-block-image aligncenter size-medium"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/support-shortcut-v2.png" alt="Stöd oss"/></figure>
+        <div class="wp-block-column"><!-- wp:group {"className":"is-clickable-shortcut","layout":{"type":"constrained"}} -->
+            <div class="wp-block-group is-clickable-shortcut"><!-- wp:image {"sizeSlug":"medium","linkDestination":"none","align":"center"} -->
+                <figure class="wp-block-image aligncenter size-medium"><img src="http://localhost:32781/wp-content/themes/efs/assets/images/support-shortcut-v2.png" alt="Stöd oss" /></figure>
                 <!-- /wp:image -->
-                <!-- wp:paragraph {"align":"center"} -->
+
+                <!-- wp:paragraph {"style":{"typography":{"textAlign":"center"}}} -->
                 <p class="has-text-align-center">Stöd oss</p>
                 <!-- /wp:paragraph -->
             </div>
@@ -40,13 +36,12 @@
         <!-- /wp:column -->
 
         <!-- wp:column -->
-        <div class="wp-block-column">
-            <!-- wp:group {"className":"is-clickable-shortcut","layout":{"type":"constrained"}} -->
-            <div class="wp-block-group is-clickable-shortcut">
-                <!-- wp:image {"align":"center","sizeSlug":"medium","linkDestination":"none","width":200,"height":200} -->
-                <figure class="wp-block-image aligncenter size-medium"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/newsletter-shortcut-v2.png" alt="Nyhetsbrev"/></figure>
+        <div class="wp-block-column"><!-- wp:group {"className":"is-clickable-shortcut","layout":{"type":"constrained"}} -->
+            <div class="wp-block-group is-clickable-shortcut"><!-- wp:image {"sizeSlug":"medium","linkDestination":"none","align":"center"} -->
+                <figure class="wp-block-image aligncenter size-medium"><img src="http://localhost:32781/wp-content/themes/efs/assets/images/newsletter-shortcut-v2.png" alt="Nyhetsbrev" /></figure>
                 <!-- /wp:image -->
-                <!-- wp:paragraph {"align":"center"} -->
+
+                <!-- wp:paragraph {"style":{"typography":{"textAlign":"center"}}} -->
                 <p class="has-text-align-center">Nyhetsbrev</p>
                 <!-- /wp:paragraph -->
             </div>
